ef586edb98a54b4a83903ce54348bc1e1e69a110
Kreativan <kreativan.dev@gmail.com>
2022-01-28 15:51:22 +0200

    htmx file description update

--- site-wirekit-core/templates/htmx.php
+++ site-wirekit-core/templates/htmx.php
@@ -1,35 +1,38 @@
 <?php namespace ProcessWire;
 /**
- *  This is where dynamic content is rendered
+ *  HTMX end-point
  *  This page should always return html
- *
- *  Use @var $htmx->url to get the html root url
- *
- *  You can access this page on: {$htmx->url}folder/file/
- *
+ *  Use @var $htmx->url as end-point root url
+ *  
+ *  Get content with url segments: {$htmx->url}folder/file/
  *  @example {$htmx->url}layout/product/card/
  *  will render /layout/product/card.php
  * 
- *  Any file up to 4 levels, from allowed paths
- *  @example {$htmx->url}folder/sub_1/sub_2/sub_3/file
+ *  Any file up to 4 segments from allowed paths are supported
+ *  @example {$htmx->url}folder/sub_1/sub_2/sub_3/file/
  * 
+ *  # $_GET
  *  All $_GET variables are passed as data to the file
  *  @example {$htmx->url}layout/my-file/?key=value 
  *  @example echo $key
  * 
- *  Pages
- *  $page is automatically passed as data to the render files
+ *  # Pages
+ *  Use page segment to render the page
+ *  @example {$htmx->url}page/1047/
  *  Add ?htmx=1 to exclude header and footer
  *  @example {$htmx->url}page/1047/?htmx=1
- *  @example {$htmx->url}page/?page_id=1047
- *  @example {$htmx->url}page/?page_url=1047
- *  @example {$htmx->url}page/?page_url=1047&page_ref=product
  * 
- *  Page Reference
+ *  # Page
+ *  Pass the page object to the the render file with page_id or page_url
+ *  @example {$htmx->url}page/?page_id=1047
+ *  @example {$htmx->url}page/?page_url=/basic-page/
+ * 
+ *  # Page Reference
  *  Add page_ref variable to reference the $page
  *  in this example $page and $product will be the same
  *  @example {$htmx->url}/layout/card/?page_id=123&page_ref=product
- * 
+ *  
+ *  @see /classes/HtmxPage.php for more info
  */
 
 require_once("_init.php");
